8f18854dacff71d6ec22c307115708f5c5174e68
Joydeep Pal <joy.xmofg@gmail.com>
2023-09-16 03:07:03 +0530

    updated script

--- enable-shaper.sh
+++ enable-shaper.sh
@@ -5,6 +5,7 @@
 # Description:	Traffic Manager Configuration for setting Shaper rates 
 # 		and then enabling Shaper.
 
+echo 'Enabling Level 2 shapers 0-3 and Level 0 shaper'
 nfp-reg xpb:Nbi0IsldXpbMap.NbiTopXpbMap.TrafficManager.TMShaperReg.ShaperRate{{0..3},144}.Rate=0x3fff
 nfp-reg xpb:Nbi0IsldXpbMap.NbiTopXpbMap.TrafficManager.TMShaperReg.ShaperRate2.Rate=1
 nfp-reg xpb:Nbi0IsldXpbMap.NbiTopXpbMap.TrafficManager.TMShaperReg.ShaperRate144.Rate=2
